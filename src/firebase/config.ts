cdd6744ea22cb2b0a62590fc78934dac36356ff1
bidhaayath thoiba <bidhaayath@gmail.com>
2026-03-31 14:52:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2401149755-66e8a",
+  "appId": "1:299498292665:web:8dc5809aa6f8e429d16268",
+  "apiKey": "AIzaSyDhthHp0H0syj-3sQYyNAO9gkAqttHgLPA",
+  "authDomain": "studio-2401149755-66e8a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "299498292665"
+};
